6fca77f3c96c09764e2cdb6abb327d9706c61990
ASPiRe <afoninsergey@mail.ru>
2022-10-11 18:12:23 +0300

    Third modofocation

--- js/script.js
+++ js/script.js
@@ -1,64 +1,83 @@
-/* Задания на урок:
-
-1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
-новый фильм добавляется в список. Страница не должна перезагружаться.
-Новый фильм должен добавляться в movieDB.movies.
-Для получения доступа к значению input - обращаемся к нему как input.value;
-P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
-
-2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
-
-3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
-
-4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
-"Добавляем любимый фильм"
-
-5) Фильмы должны быть отсортированы по алфавиту */
-
 "use strict";
 
 document.addEventListener("DOMContentLoaded", () => {
-    const movieDB = {
-        movies: [
-            "Логан",
-            "Лига справедливости",
-            "Ла-ла лэнд",
-            "Одержимость",
-            "Скотт Пилигрим против...",
-            "Антей гонит",
-        ],
-        },
-        banners = document.querySelectorAll(".promo__adv img"),
-        promo = document.querySelector(".promo__bg"),
-        genre = promo.querySelector(".promo__genre"),
-        promoList = document.querySelector(".promo__interactive-list");
+  //START
+  const movieDB = {
+      movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против...",
+        "Антей гонит",
+      ],
+    },
+    banners = document.querySelectorAll(".promo__adv img"),
+    promo = document.querySelector(".promo__bg"),
+    genre = promo.querySelector(".promo__genre"),
+    promoList = document.querySelector(".promo__interactive-list"),
+    movieForm = document.querySelector(".add"),
+    movieInput = movieForm.querySelector(".adding__input"),
+    checkbox = movieForm.querySelector('[type="checkbox"]');
+  let bascets;
 
-    function adb(arr) {
-        arr.forEach((banner) => {
-        banner.remove();
-        });
-    }
-    adb(banners);
+  function adb(arr) {
+    arr.forEach((banner) => {
+      banner.remove();
+    });
+  }
+  adb(banners);
 
-    function setGenre(text) {
-        genre.textContent = text;
-    }
-    setGenre("драма");
+  function setGenre(text) {
+    genre.textContent = text;
+  }
+  setGenre("драма");
 
-    function setPromoBg(url) {
-        promo.style.backgroundImage = url;
-    }
-    setPromoBg("url('img/bg.jpg')");
+  function setPromoBg(url) {
+    promo.style.backgroundImage = url;
+  }
+  setPromoBg("url('img/bg.jpg')");
 
-    function setInteractiveList(list, movies) {
-        list.innerHTML = "";
-        movies.sort().forEach((movie, i) => {
-        list.innerHTML += `
-                    <li class="promo__interactive-item">${i+1}. ${movie}
+  function setInteractiveList(list, movies) {
+    list.innerHTML = "";
+    movies.sort().forEach((movie, i) => {
+      list.innerHTML += `
+                    <li class="promo__interactive-item">${i + 1}. ${movie}
                         <div class="delete"></div>
                     </li>
                 `;
-        });
+    });
+    bascets = document.querySelectorAll(".delete").forEach((bascet, i) => {
+      bascet.addEventListener("click", () => {
+        // delete movies[i]; // УДАЛЯЕТ НО ОСТАВЛЯЕТ ПУСТУЮ ЯЧЕЙКУ
+        movies.splice(i, 1);  
+
+        setInteractiveList(promoList, movies);
+      });
+    });
+  }
+  setInteractiveList(promoList, movieDB.movies);
+
+  function addMovie(form, movies, input) {
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      if (input.value.trim()) {
+        if (input.value.length > 7) {
+          input.value = `${input.value.slice(0, 7)}...`;
+        }
+        if (checkbox.checked) {
+          console.log("Добавляем любимый фильм");
+        }
+        movies.push(input.value);
+        e.target.reset(); // form
+        setInteractiveList(promoList, movies);
+      } else {
+        e.target.reset();
+        input.placeholder = "Введите название";
     }
-    setInteractiveList(promoList, movieDB.movies);
+    });
+  }
+  addMovie(movieForm, movieDB.movies, movieInput);
+
+  //END
 });
